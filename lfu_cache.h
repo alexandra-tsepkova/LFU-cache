ca92f86e7decfb8b1ca479854c76e8d6cf01f213
Alexandra Tsepkova <tsepkova.ai@phystech.edu>
2020-09-27 15:49:03 +0300

    fixed syntaxis + input format

--- lfu_cache.h
+++ lfu_cache.h
@@ -11,83 +11,90 @@
 #include <unordered_map>
 
 
-template <typename T> class tree_node {
-    T page;
-    unsigned int freq;
+namespace caches {
 
-public:
-    tree_node(T page){
-        this->page = page;
-        this->freq = 0;
-    }
-    void increment_freq(){
-        freq++;
-    }
-    T get_page() {
-        return page;
-    }
-    bool operator<(const tree_node &rv) const {
-        return (freq == rv.freq) ? (page < rv.page) : (freq < rv.freq);
-    }
-};
+    template<typename T>
+    class tree_node {
+        T page;
+        unsigned int freq;
 
-template <typename T>
-class pointer_less {
-public: bool operator() (const tree_node<T> *lv, const tree_node<T> *rv)  const {
-        return (*lv) < (*rv);
-    }
-};
+    public:
+        tree_node(T page) : page(page), freq(0) {}
 
-template <typename T> class lfu_cache {
-    std::set<tree_node<T>*, pointer_less<T>> cache = std::set<tree_node<T>*, pointer_less<T>>();
-    std::unordered_map<T, tree_node<T>*> hash_table = std::unordered_map<T, tree_node<T>*>();
-    size_t max_size;
-
-public:
-    lfu_cache(size_t max_size){ //constructor
-        this->max_size = max_size;
-    }
-    void print_set() {
-        std::cout << "set" << std::endl;
-        for (const auto &elem : cache) {
-            std::cout << ">  " << elem->get_page() << std::endl;
+        void increment_freq() {
+            freq++;
         }
-    }
 
-    bool is_in_cache(T page){
-        assert(hash_table.size() == cache.size());
-        auto search_res = hash_table.find(page);
-        if (search_res != hash_table.end()) {
-            tree_node<T> *cur_node = search_res->second;
-            cache.erase(cur_node);
-            cur_node->increment_freq();
-            cache.insert(cur_node);
-            return true;
-        } else {
-            if (is_full() == true) {
-                auto min_node = cache.begin(); // upper_bound(zero_freq);
-                if (min_node != cache.end()) {
-                    T min_page = (*min_node)->get_page();
-                    delete *min_node; // Deletes object pointed by *min_node
-                    cache.erase(
-                            min_node); // Deletes pointer from cache and invalidates all iterators including min_node
-                    hash_table.erase(min_page);
-                }
+        T get_page() const {
+            return page;
+        }
+
+        bool operator<(const tree_node &rv) const {
+            return (freq == rv.freq) ? (page < rv.page) : (freq < rv.freq);
+        }
+    };
+
+    template<typename T>
+    class pointer_less {
+    public:
+        bool operator()(const tree_node<T> *lv, const tree_node<T> *rv) const {
+            return (*lv) < (*rv);
+        }
+    };
+
+    template<typename T>
+    class lfu_cache {
+        std::set<tree_node<T> *, pointer_less<T>> cache = std::set<tree_node<T> *, pointer_less<T>>();
+        std::unordered_map<T, tree_node<T> *> hash_table = std::unordered_map<T, tree_node<T> *>();
+        size_t max_size;
+
+    public:
+        lfu_cache(size_t max_size) : max_size(max_size) {}
+
+        void print_set() const {
+            std::cout << "set" << std::endl;
+            for (const auto &elem : cache) {
+                std::cout << ">  " << elem->get_page() << std::endl;
+            }
+        }
+
+        bool is_in_cache(T page) {
+            assert(hash_table.size() == cache.size());
+            auto search_res = hash_table.find(page);
+            if (search_res != hash_table.end()) {
+                tree_node<T> *cur_node = search_res->second;
+                cache.erase(cur_node);
+                cur_node->increment_freq();
+                cache.insert(cur_node);
+                return true;
+            } else {
+                if (is_full() == true) {
+                    auto min_node = cache.begin();
+                    if (min_node != cache.end()) {
+                        T min_page = (*min_node)->get_page();
+                        delete *min_node; // Deletes object pointed by *min_node
+                        cache.erase(
+                                min_node); // Deletes pointer from cache and invalidates all iterators including min_node
+                        hash_table.erase(min_page);
+                    }
 //                } else {
 //                    assert(false);
 //                }
+                }
+                auto new_node = new tree_node<T>(page);
+                new_node->increment_freq();
+                cache.insert(new_node);
+                hash_table[new_node->get_page()] = new_node;
+                return false;
             }
-            auto new_node = new tree_node<T>(page);
-            new_node->increment_freq();
-            cache.insert(new_node);
-            hash_table[new_node->get_page()] = new_node;
-            return false;
         }
-    }
-    bool is_full(){
-        return hash_table.size()>=max_size;
-    }
-};
 
+        bool is_full() const {
+            return hash_table.size() >= max_size;
+        }
+    };
+
+
+}
 
 #endif //LFU_CACHE_H
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void run_tests(){
         int lfu_size, input_num, expected_hits, actual_hits = 0, cur_num;
         f >> lfu_size >> input_num >> expected_hits;
         std::cout << "Test " << i + 1 << " ";
-        lfu_cache<int> lfu(lfu_size);
+        caches::lfu_cache<int> lfu(lfu_size);
         for (int j = 0; j < input_num; j++) {
             f >> cur_num;
             if (lfu.is_in_cache(cur_num) == true) actual_hits++;
@@ -29,14 +29,16 @@ void run_tests(){
 }
 
 int main() {
-//    lfu_cache<int> lfu(3);
-//    int hits = 0;
-//    for (int i = 0; i <= 11; ++i) {
-//        std::cout << 0 % 4 << std::endl;
-//        if (lfu.is_in_cache(0%4) == true) hits++;
+    int hits = 0, cache_size, key_num, key_cur;
+    std::cin >> cache_size >> key_num;
+    caches::lfu_cache<int> lfu(cache_size);
+
+      for (int i = 0; i < key_num; ++i) {
+          std::cin >> key_cur;
+          if (lfu.is_in_cache(key_cur) == true) hits++;
 //        lfu.print_set();
-//    }
-    run_tests();
-//    std::cout << "Hits: " << hits << std::endl;
+      }
+//    run_tests();
+    std::cout << "Hits: " << hits << std::endl;
     return 0;
 }
